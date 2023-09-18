c4d01a06d7033ea0dab3632f50e59476137bfd2d
Mercy Cherono <cheronom142@gmail.com>
2023-09-19 00:46:29 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+
+const currentUser = 'Mercy Cherono';
+
+
+// Sting interpolation with template liberals
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//.toUpperCase method
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// .slice(); method
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
